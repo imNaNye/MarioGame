14b64307b41ed19e19d34da9e2478528c0f64e58
NaNyeS2 <hyunkyungj0730@gmail.com>
2023-11-16 00:26:49 +0900

    game.java

--- Game.java
+++ Game.java
@@ -10,16 +10,58 @@ public class Game extends Canvas implements Runnable{
     private Thread thread;
     private boolean running = false;
 
-    public synchronized void start(){
-        System.out.println("jjjjjjj");
+    private synchronized void start(){
+        if(running) return;
+        running = true;
+        thread = new Thread(this, "t1");
+        thread.start();
+
     }
 
-    public synchronized void stop(){
-
+    private synchronized void stop(){
+        if (!running)  return;
+        running = false;
+        try{
+            thread.join();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void run() {
+        long lastTime = System.nanoTime();
+        long timer = System.currentTimeMillis();
+        double delta = 0.0;
+        double ns = 1000000000.0 / 60.0; //나노초 단위
+        int frames = 0; //프레임단위
+        int ticks = 0; //업데이트단위
+        while(running){ //프로그램 작동 중 실행코드
+            long now = System.nanoTime();
+            delta += (now-lastTime)/ns;
+            lastTime = now;
+            while (delta >= 1){
+                tick();
+                ticks++;
+                delta--;
+            }
+            render();
+            frames++;
+            if (System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println(frames + " frame per second " + ticks + " updates per second");
+                frames = 0;
+                ticks = 0;
+            }
+        }
+        stop();
+    }
+
+    public void render(){ // 렌더링
+
+    }
+
+    public void tick(){ // update
 
     }
 
